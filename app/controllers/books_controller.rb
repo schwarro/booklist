5aa9c878b951de6c027f7b67da15a7a43e5dc1d6
schwarro <rob.ap.schwartz@gmail.com>
2018-08-31 14:57:35 -0400

    added a json response

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,10 @@ class BooksController < ApplicationController
       format.text
       format.csv do
         render plain: Book.generate_csv(@books)
-      end 
+      end
+      format.json do
+        render json: @books
+      end
     end
   end
 end
